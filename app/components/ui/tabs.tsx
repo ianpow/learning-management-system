958435550860c8edff5f7daa9ea40c368b9b75d7
ianpow <ianpow@hotmail.com>
2025-01-24 21:27:22 +0000

    updated tabs

--- app/components/ui/tabs.tsx
+++ app/components/ui/tabs.tsx
@@ -1,46 +1,48 @@
 // app/components/ui/tabs.tsx
 'use client'
 
-import * as TabsPrimitive from '@radix-ui/react-tabs'
-import React from 'react'
+import * as React from "react"
+import * as TabsPrimitive from "@radix-ui/react-tabs"
 
 const Tabs = TabsPrimitive.Root
 
-interface TabsListProps extends React.HTMLAttributes<HTMLDivElement> {}
-interface TabsTriggerProps extends TabsPrimitive.TabsTriggerProps {}
-interface TabsContentProps extends TabsPrimitive.TabsContentProps {}
+interface TabsProps extends React.HTMLAttributes<HTMLDivElement> {
+  value?: string;
+  className?: string;
+}
 
-const TabsList = React.forwardRef<HTMLDivElement, TabsListProps>(
- (props, ref) => (
-   <TabsPrimitive.List
-     ref={ref}
-     className="flex gap-4 border-b border-gray-200"
-     {...props}
-   />
- )
-)
-TabsList.displayName = 'TabsList'
+const TabsList = React.forwardRef<HTMLDivElement, TabsProps>((props, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={`inline-flex h-10 items-center justify-center rounded-md bg-slate-100 p-1 ${props.className || ''}`}
+    {...props}
+  />
+))
+TabsList.displayName = "TabsList"
 
-const TabsTrigger = React.forwardRef<HTMLButtonElement, TabsTriggerProps>(
- (props, ref) => (
-   <TabsPrimitive.Trigger
-     ref={ref}
-     className="px-3 py-2 text-sm font-medium border-b-2 border-transparent hover:border-gray-300 data-[state=active]:border-blue-600"
-     {...props}
-   />
- )
-)
-TabsTrigger.displayName = 'TabsTrigger'
+const TabsTrigger = React.forwardRef<HTMLButtonElement, TabsPrimitive.TabsTriggerProps>((props, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
+    className={`inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium transition-all focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-900 data-[state=active]:shadow-sm ${props.className || ''}`}
 
-const TabsContent = React.forwardRef<HTMLDivElement, TabsContentProps>(
- (props, ref) => (
-   <TabsPrimitive.Content
-     ref={ref}
-     className="mt-4"
-     {...props}
-   />
- )
-)
-TabsContent.displayName = 'TabsContent'
+    {...props}
+  />
+))
+TabsTrigger.displayName = "TabsTrigger"
 
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+const TabsContent = React.forwardRef<HTMLDivElement, TabsPrimitive.TabsContentProps>((props, ref) => (
+  <TabsPrimitive.Content
+    ref={ref}
+    className={`mt-2 ${props.className || ''}`}
+
+    {...props}
+  />
+))
+TabsContent.displayName = "TabsContent"
+
+export {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent
+}
